4a69782ca873e60e578d2ee36a49109093a72029
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-09 23:56:30 +0530

    Add files via upload

---
+++ src/test/java/com/audio/audio_tanscribe/AudioTanscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audio_tanscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTanscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
