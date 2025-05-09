37201905f1e5f043e9574578e8b8a34cc0175316
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-10 00:05:01 +0530

    Save local changes before pulling

--- src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
+++ src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
@@ -1,4 +1,11 @@
 package com.audio.audio_tanscribe;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transcription")
 public class TranscriptionController {
+
+
 }
