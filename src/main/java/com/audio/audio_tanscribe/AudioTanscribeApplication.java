23053bcd01e75615c24b213b57ce85f0f8473634
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-09 23:37:02 +0530

    /

---
+++ src/main/java/com/audio/audio_tanscribe/AudioTanscribeApplication.java
@@ -0,0 +1,13 @@
+package com.audio.audio_tanscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTanscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTanscribeApplication.class, args);
+	}
+
+}
