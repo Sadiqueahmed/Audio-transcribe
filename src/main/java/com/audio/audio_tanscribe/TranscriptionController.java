579a670239cc5ae1b5376df321af48ee90fba948
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-10 12:22:25 +0530

    /

--- src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
+++ src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
@@ -1,10 +1,23 @@
 package com.audio.audio_tanscribe;
 
 
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/transcription")
@@ -12,13 +25,33 @@ public class TranscriptionController {
 
     private final OpenAiAudioTranscriptionModel transcriptionModel;
 
-    public TranscriptionController(String apiKey) {
-        OpenAiAudioApi openAiAudioApi
-                = new OpenAiAudioApi(System.getenv("spring.ai.open.api-key"));
+    public TranscriptionController(@Value("${spring.ai.api-key}") String apiKey) {
+        OpenAiAudioApi openAiAudioApi = new OpenAiAudioApi(apiKey);
 
-        this.transcriptionModel
-                = new OpenAiAudioTranscriptionModel(openAiAudioApi);
+        this.transcriptionModel = new OpenAiAudioTranscriptionModel(openAiAudioApi);
     }
+
+    public ResponseEntity<String> transcribeAudio(
+          @RequestParam("file") MultipartFile file) throws IOException {
+        File tempFile = File.createTempFile("audio", ".wav");
+        file.transferTo(tempFile);
+
+        OpenAiAudioTranscriptionOptions transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .language("en-US")
+                .temperature(0f)
+                .build();
+
+        FileSystemResource audioFile = new FileSystemResource(tempFile);
+
+        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audioFile, transcriptionOptions);
+        AudioTranscriptionResponse response = transcriptionModel.call(transcriptionRequest);
+
+        tempFile.delete();
+        return new ResponseEntity<>(response.getResult().getOutput(), HttpStatus.OK);
+
+    }
+
 
 
 
