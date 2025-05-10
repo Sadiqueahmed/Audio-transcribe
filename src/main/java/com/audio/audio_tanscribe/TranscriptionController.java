51dac52e44495fe70b520f3dff3ceed3917722d8
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-05-10 11:15:38 +0530

    /

--- src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
+++ src/main/java/com/audio/audio_tanscribe/TranscriptionController.java
@@ -1,4 +1,25 @@
 package com.audio.audio_tanscribe;
 
+
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transcription")
 public class TranscriptionController {
+
+    private final OpenAiAudioTranscriptionModel transcriptionModel;
+
+    public TranscriptionController(String apiKey) {
+        OpenAiAudioApi openAiAudioApi
+                = new OpenAiAudioApi(System.getenv("spring.ai.open.api-key"));
+
+        this.transcriptionModel
+                = new OpenAiAudioTranscriptionModel(openAiAudioApi);
+    }
+
+
+
 }
